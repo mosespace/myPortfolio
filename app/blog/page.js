e57b6ae0bf3302159f1793f3c6c604f739b2e2b3
mosespace <kisakyemoses50@gmail.com>
2023-07-17 14:34:19 +0300

    feat: added metadata to the page

---
+++ app/blog/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export const metadata = {
+    title: "Blog",
+    alternates: {
+      canonical: "/blog",
+    },
+  };
+
+export default function page() {
+  return (
+    <div>page</div>
+  )
+}
