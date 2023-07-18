ddf3f27ae3a423353cc1a7faa2a711a50183a827
mosespace <kisakyemoses50@gmail.com>
2023-07-18 22:56:13 +0300

    feat: added use client

--- components/BlogData.jsx
+++ components/BlogData.jsx
@@ -1,43 +1,42 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { FaFacebookSquare, FaTwitterSquare } from "react-icons/fa";
-import { motion, useScroll } from "framer-motion";
 
 export default function BlogData({ blog }) {
-    const { scrollYProgress } = useScroll();
-
-
   return (
-    <motion.div className='all-content progress-bar' style={{ scaleX: scrollYProgress }}>
-      <h1>{blog.detailedTitle}</h1>
-      <h3>
-        by {blog.author}, {blog.jobTitle}
-      </h3>
-      <h3 className='date'>{blog.date}</h3>
-      <div className='main-blog-text'>
-        <p className='first-paragraph'>{blog.firstParagraph}</p>
+    <>
+      <div className='all-content'>
+        <h1>{blog.detailedTitle}</h1>
+        <h3>
+          by {blog.author}, {blog.jobTitle}
+        </h3>
+        <h3 className='date'>{blog.date}</h3>
+        <div className='main-blog-text'>
+          <p className='first-paragraph'>{blog.firstParagraph}</p>
 
-        <h2 className='sub-title1'>{blog.subTitle}y</h2>
+          <h2 className='sub-title1'>{blog.subTitle}y</h2>
 
-        <p className='second-paragraph'>{blog.secondParagraph}</p>
+          <p className='second-paragraph'>{blog.secondParagraph}</p>
 
-        <h2 className='sub-title2'>{blog.subTitle2}</h2>
+          <h2 className='sub-title2'>{blog.subTitle2}</h2>
 
-        <p className='third-paragraph'>{blog.thirdParagraph}</p>
-        <Link href='' className='banner-img'>
-          <Image
-            src={blog.bannerImage}
-            alt='Image Only'
-            width={700}
-            height={200}
-          />
-        </Link>
+          <p className='third-paragraph'>{blog.thirdParagraph}</p>
+          <Link href='' className='banner-img'>
+            <Image
+              src={blog.bannerImage}
+              alt='Image Only'
+              width={700}
+              height={200}
+            />
+          </Link>
+        </div>
+        <div className='share-links'>
+          <h1>SHARE THIS BLOG</h1>
+          <FaTwitterSquare size={30} className='icons' />
+          <FaFacebookSquare size={30} className='icons' />
+        </div>
       </div>
-      <div className='share-links'>
-        <h1>SHARE THIS BLOG</h1>
-        <FaTwitterSquare size={30} className='icons' />
-        <FaFacebookSquare size={30} className='icons' />
-      </div>
-    </motion.div>
+    </>
   );
 }
