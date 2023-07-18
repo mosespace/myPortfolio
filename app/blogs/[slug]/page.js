bb40a6529fa7c09b50ab19e0aec5ab7c94a82c4f
mosespace <kisakyemoses50@gmail.com>
2023-07-18 22:34:03 +0300

    feat added detailed page

---
+++ app/blogs/[slug]/page.js
@@ -0,0 +1,39 @@
+import getBlogs from "@/app/lib/getBlogs";
+import BlogData from "@/components/BlogDAta";
+import SwiperImage from "@/components/SwiperImage";
+import React from "react";
+
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const { slug } = params;
+  // fetch data
+  const blogs = await getBlogs();
+  const blog = blogs?.find((blog) => blog.slug == slug);
+
+  return {
+    title: blog.title,
+    description: blog.overview,
+    alternates: {
+      canonical: `/blogs/${blog.slug}`,
+    },
+  };
+}
+
+export default async function Page({ params: { slug } }) {
+  const blogs = await getBlogs();
+
+  const blog = blogs?.find((blog) => blog.slug == slug);
+
+  return (
+    <div className='full-blog-display'>
+      <div className='image-container'>
+        <SwiperImage blogs={blog} />
+      </div>
+
+      <dayiv className='all-content'>
+        <BlogData blog={blog}/>
+      </dayiv>
+    </div>
+  );
+}
