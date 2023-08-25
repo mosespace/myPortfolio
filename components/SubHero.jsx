76c7ef0a66851c9ca21f0911cbc4d519ab1e561f
mosespace <kisakyemoses50@gmail.com>
2023-08-25 16:35:17 +0300

    feat: created modifications for the subHero component

--- components/SubHero.jsx
+++ components/SubHero.jsx
@@ -5,37 +5,14 @@ import myImage from "../public/imgbg.png";
 export default function SubHero() {
   return (
     <section className='sub-hero'>
-      <div className="hero-content">
+      <div className='hero-content'>
         {/* <div className="bg-image">
         <Image src={myImage} alt="Title" fill />
       </div> */}
-        <h1 className="main-title">Programming and design from the future</h1>
-        <button className="btn-outline">Dig Into My Universe</button>
-      
+        <h1 className='main-title'>Programming and design from the future</h1>
+        <a href="#mainBody-text">
+        <button className='btn-outline'>Dig Into My Universe</button></a>
       </div>
-      {/* <div className='hero-flex'>
-        <div className='hero-content'>
-          <h1>Hello I am Kisakye Moses</h1>
-          <h3>
-            Am a software <span>Dev.</span> and a UI/UX Designer.
-          </h3>
-          <p className='sub-para'>
-            I've been designing web applications for over 7+ years and am super
-            experienced as a <span>FrontEnd Developer 😎</span>
-          </p>
-          <div className='action-buttons'>
-            <button className='btns-1'>Hire Me</button>
-            <button className='btns-2'>Projects</button>
-          </div>
-        </div>
-        <div className='my-image'>
-          <Image
-            src={myImage}
-            alt='Kisakye Moses, Software Developerr In Uganda'
-            fill
-          />
-        </div>
-      </div> */}
     </section>
   );
 }
